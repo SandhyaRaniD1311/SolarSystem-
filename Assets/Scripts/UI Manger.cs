c03bb194d4f932567dfb369bdd7b94a33fafe316
SandhyaRaniD1311 <sandhyaranid13112002@gmail.com>
2024-06-15 14:54:23 +0530

    First Commit

---
+++ Assets/Scripts/UI Manger.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManger : MonoBehaviour
+{
+    public GameObject MenuPanel;
+    public GameObject LoadingPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadingPanel.SetActive(false);
+        MenuPanel.SetActive(true);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    IEnumerator DElaySceneLoad()
+    {
+        LoadingPanel.SetActive(true);
+        MenuPanel.SetActive(false);
+        yield return new WaitForSeconds(8);
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExploreArCall()
+    {
+        StartCoroutine(DElaySceneLoad());
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
